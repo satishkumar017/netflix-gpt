e58db0ad9276efe395879550fb732504ec3bddb0
Satish Kumar N <satishari7@gmail.com>
2026-03-24 21:53:30 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="bg-black  h-screen flex items-center justify-center">
+      <h1 className="text-red-600 text-5xl font-bold">
+        Netflix GPT 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App;
+
